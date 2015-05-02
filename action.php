3b15a4cb985806a84cd6124d4d8b7e010aa13677
Julien ABADJI <aulien.jbadji@gmail.com>
2015-05-02 18:40:45 +0200

    Commencage du action.php qui gere signup et login pour l'instant.

---
+++ action.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once './lib/evaluator.inc.php'; 
+switch($_GET['a']){
+	case 'sign':
+	  $user['nick'] = htmlspecialchars($_POST['nick']);
+      $user['mail'] = $_POST['mail'];
+      $user['pass'] = password_hash($_POST['pass'], PASSWORD_BCRYPT); //WARNING : 72chars max.
+      if(createUser($user)){
+      	header('location:' . $_SERVER['HTTP_REFERER']);
+      }
+		break;
+	case 'log':
+		$user['nick'] = htmlspecialchars($_POST['nick']);
+		$user['pass'] = $_POST['pass'];
+		if(login($user)){
+			$_SESSION['nick'] = $_POST['nick'];
+			$_SESSION['mail'] = $_POST['mail'];
+			header('location:' . $_SERVER['HTTP_REFERER']);
+		}
+	default:
+		break;
+
+}
+?>
